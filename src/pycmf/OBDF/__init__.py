4526543d3f5376e5faae9714867147c18f3dc3f8
PhungNgocDuy <127763027+PhungNgocDuy@users.noreply.github.com>
2026-03-14 16:13:29 +0700

    Delete "#from .dfuobmp2_old import DFUOBMP2 as DFUOBMP2_old"

--- src/pycmf/OBDF/__init__.py
+++ src/pycmf/OBDF/__init__.py
@@ -12,7 +12,6 @@ from .dfuobmp2 import DFUOBMP2
 from .dfuobmp2_einsum import DFUOBMP2 as DFUOBMP2_einsum
 from .dfuobmp2_mom import DFUOBMP2 as DFUOBMP2_mom
 from .dfuobmp2_mom_diis import DFUOBMP2 as DFUOBMP2_mom_diis
-#from .dfuobmp2_old import DFUOBMP2 as DFUOBMP2_old
 
 __all__ = [
     'DFOBMP2', 'DFOBMP2_slow',
